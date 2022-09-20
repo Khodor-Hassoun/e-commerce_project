c489cec6f47c342cb66b41e45a61c73b3afd5050
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-20 12:18:35 +0300

    Admin can view all clients (buyers)

---
+++ backend-admin/getUsersClients.php
@@ -0,0 +1,18 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    include("connection.php");
+
+    $query = $mysqli->prepare("SELECT * FROM users WHERE user_types_id =3");
+    $query->execute();
+    $array = $query->get_result();
+
+    $response = [];
+
+    while($a = $array->fetch_assoc()){
+        $response[] = $a;
+    }
+
+    $json = json_encode($response);
+    echo $json;
+
+?>
