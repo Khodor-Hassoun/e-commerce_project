8f3de314a2b07cb5e3c2d094207725387063d580
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-20 17:56:51 +0300

    Nsit sho 8ayaret bas sar fi edit idk

--- backend-admin/addSeller.php
+++ backend-admin/addSeller.php
@@ -5,13 +5,33 @@
     // To add a seller they must be registered and already exist in the db
     $userId = $_POST['q'];
     $userTypeSeller = 2;
-
-    if (isset($userId) && !empty($userId)){
-        $query = $mysqli->prepare("UPDATE users SET user_types_id =? WHERE id =?");
-        $query ->bind_param('ii',$userTypeSeller,$userId);
-        $query ->execute();
-        echo json_encode(['success'=>'True']);
+    $userTypeBuyer = 3;
+    // I will now select the user to check if they exist
+    $query = $mysqli->prepare('SELECT * FROM users WHERE id=? AND user_types_id =?');
+    $query ->bind_param('ii', $userId, $userTypeBuyer);
+    $query->execute();
+    $query ->store_result();
+    $num_rows = $query->num_rows();
+    if($num_rows == 0){
+        echo 'All are sellers';
+    }else{
+        if (isset($userId) && !empty($userId)){
+            $query = $mysqli->prepare("UPDATE users SET user_types_id =? WHERE id =?");
+            $query ->bind_param('ii',$userTypeSeller,$userId);
+            $query ->execute();
+            echo json_encode(['success'=>'True']);
+        }
     }
+
+
+
+    // if (isset($userId) && !empty($userId)){
+    //     $query = $mysqli->prepare("UPDATE users SET user_types_id =? WHERE id =?");
+    //     $query ->bind_param('ii',$userTypeSeller,$userId);
+    //     $query ->execute();
+    //     echo json_encode(['success'=>'True']);
+    // }
+
     // $query = $mysqli->prepare("UPDATE users SET user_types_id =? WHERE id =?");
     // $query ->bind_param('ii',$userTypeSeller,$userId);
     // $query ->execute();
