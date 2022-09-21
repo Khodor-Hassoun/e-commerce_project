711cd07cc277e6734b1e6d364f9dc576b76fda52
HawraaYounes <hawraayounes0@gmail.com>
2022-09-21 15:41:53 +0300

    Adding addProductByCategory API

--- backend_seller/deleteProduct.php
+++ backend_seller/deleteProduct.php
@@ -1,9 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-
+include("headers/headers.php");
 include("connection/connection.php");
 
 $product_id = $_GET["product_id"];
