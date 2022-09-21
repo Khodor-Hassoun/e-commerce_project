008f46ae69429556b41eaaddff4b0047db82da9d
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-21 15:34:51 +0300

    getting best client of the year

--- backend-admin/getBestClient.php
+++ backend-admin/getBestClient.php
@@ -54,4 +54,25 @@
     $query->execute();
     $response["best client per month"] = $query->get_result()->fetch_assoc();
 
+    //Prepare and execute query to get best client of the month based on how much they paid
+    $query = $mysqli->prepare(
+
+        "SELECT  
+            orders_count.client_id, 
+            MAX(orders_count.orders_made)
+        FROM ( SELECT 
+                    U.ID AS client_id, 
+                    sum(O.total_price) AS orders_made 
+                FROM orders O  
+                INNER JOIN users U ON U.ID = O.client_id 
+                WHERE U.USER_TYPE = 2 
+                AND O.CREATED_AT between now() - INTERVAL 1 year and now()
+                Group by U.ID ) orders_count;"
+    );
+
+    $query->execute();
+    $response["best client per year"] = $query->get_result()->fetch_assoc();
+
+    echo json_encode($response);
+
 ?>
