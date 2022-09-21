8a9ffc8930c0c20e99e59459b37ee6b7ded00b19
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-21 15:24:55 +0300

    getting best seller of the year

--- backend-admin/getBestSeller.php
+++ backend-admin/getBestSeller.php
@@ -57,4 +57,27 @@
     $query->execute();
     $response["best seller per month"] = $query->get_result()->fetch_assoc();
 
+    //Prepare and execute query to get best seller of the year based on how many items are sold
+    $query = $mysqli->prepare(
+
+        "SELECT  
+            products_count.seller_id, 
+            MAX(products_count.SOLD_products)
+        FROM ( SELECT 
+                    U.ID AS seller_id, 
+                    sum(O.quantity) AS SOLD_products 
+                FROM orders_catalog O 
+                INNER JOIN products P ON P.ID = O.product_id 
+                INNER JOIN categories C ON C.ID = P.CATEGORY_ID 
+                INNER JOIN users U ON U.ID = C.SELLER_ID 
+                WHERE U.USER_TYPE = 2 
+                AND O.CREATED_AT between now() - INTERVAL 1 year and now()
+                Group by U.ID ) products_count;"
+    );
+
+    $query->execute();
+    $response["best seller per year"] = $query->get_result()->fetch_assoc();
+
+    echo json_encode($response);
+
 ?>
