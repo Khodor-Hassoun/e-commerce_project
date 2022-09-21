b0963e1c6ad3a51fb88922c46c4a84cd92f59d52
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-21 16:13:29 +0300

    link login client

--- frontend-admin/js/login_script.js
+++ frontend-admin/js/login_script.js
@@ -1,7 +1,26 @@
+//Initialize APIs
+const loginAPI = "http://localhost/e-commerce/login.php";
+const axios = require('axios').default;
 const button = document.getElementById("login_btn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const error = document.getElementById("erorr_message");
 
-const redirect = () => {
-  window.location.replace("./seller.html");
+const login = () => {
+  axios.post(loginAPI, {
+    email: `${email.value}`,
+    password: `${password.value}`
+  })
+  .then(function (response) {
+     if(response.error != NULL ){
+      error.textContent = response.error;
+      return;
+    }
+
+    window.location.replace("./seller.html");
+    localStorage.setItem("userID", response.id)
+    }
+  )
 }
-
-button.addEventListener("click", redirect);
+  
+button.addEventListener("click", login);
