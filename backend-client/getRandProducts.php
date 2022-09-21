9a3dafa41aefb90cbf0c296490512d7078413d9f
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-21 17:07:51 +0300

    checking for response error

--- backend-client/getRandProducts.php
+++ backend-client/getRandProducts.php
@@ -13,4 +13,20 @@
 
     $response = $query->get_result()->fetch_assoc();
 
+    //If no response was given, send back an error message
+    if (empty($response)) {
+        http_response_code(400);
+        echo json_encode([
+            'error' => 400,
+            'message' => 'unable to retrieve products'
+        ]);
+
+        return;
+    }
+
+    echo json_encode($response);
+
+    $query->close();
+    $mysqli->close();
+
 ?>
