4d0cc5c81aa47a743c9727fe86e77b4065079cb8
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-21 15:30:44 +0300

    getting best client of the week

--- backend-admin/getBestClient.php
+++ backend-admin/getBestClient.php
@@ -16,4 +16,24 @@
         return;   
     }
 
+    //Prepare and execute query to get best client of the week based on how much they paid
+    $query = $mysqli->prepare(
+
+        "SELECT  
+            orders_count.client_id, 
+            MAX(orders_count.orders_made)
+        FROM ( SELECT 
+              U.ID AS client_id, 
+              sum(O.total_price) AS orders_made 
+              FROM orders O  
+              INNER JOIN users U ON U.ID = O.user_id 
+              WHERE U.USER_TYPE = 2 
+              AND O.CREATED_AT between now() - INTERVAL 1 week and now()
+              Group by U.ID ) orders_count;"
+    );
+
+    $query->execute();
+    $response["best client per week"] = $query->get_result()->fetch_assoc();
+
+
 ?>
