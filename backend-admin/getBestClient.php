2d8819c6e93ab0ab1761ec650aeacddaa087293a
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-21 15:32:41 +0300

    getting best client of the month

--- backend-admin/getBestClient.php
+++ backend-admin/getBestClient.php
@@ -35,5 +35,23 @@
     $query->execute();
     $response["best client per week"] = $query->get_result()->fetch_assoc();
 
+    //Prepare and execute query to get best client of the month based on how much they paid
+    $query = $mysqli->prepare(
+
+        "SELECT  
+            orders_count.client_id, 
+            MAX(orders_count.orders_made)
+        FROM ( SELECT 
+                    U.ID AS client_id, 
+                    sum(O.total_price) AS orders_made 
+                FROM orders O  
+                INNER JOIN users U ON U.ID = O.user_id 
+                WHERE U.USER_TYPE = 2 
+                AND O.CREATED_AT between now() - INTERVAL 1 month and now()
+                Group by U.ID ) orders_count;"
+    );
+
+    $query->execute();
+    $response["best client per month"] = $query->get_result()->fetch_assoc();
 
 ?>
