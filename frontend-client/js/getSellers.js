c2755fc976de03887b163cbf07ef69d600d8363e
HawraaYounes <hawraayounes0@gmail.com>
2022-09-25 00:00:10 +0300

    Passing seller id to getSellerCategories function on click

--- frontend-client/js/getSellers.js
+++ frontend-client/js/getSellers.js
@@ -1,9 +1,9 @@
 const sellerContainer=document.querySelector(".sellers");
 const getSellerAPI="http://localhost/backend/getSellers.php";
+const getSellerCategoriesAPI="http://localhost/backend/getSellers.php";
 const getSellers=()=>{
     axios.get(getSellerAPI)
     .then(response =>  {
-        console.log(response.data)
         //Show error
         if (response.error != null) {
             console.log("error")
@@ -15,7 +15,32 @@ const getSellers=()=>{
             img.classList.add("seller");
             img.src = '';
             sellerContainer.appendChild(img);
+            img.addEventListener("click",function(){
+                getSellerCategories(response.data[i].id)
+            },false)
         }
     });
+}
+
+const getSellerCategories=(id)=>{
+    axios.get(getSellerAPI)
+    .then(response =>  {
+        //Show error
+        if (response.error != null) {
+            console.log("error")
+            return
+        }
+        //Loop over the response
+        for(let i = 0; i < response.data.length; i++){
+            const img=document.createElement("img");
+            img.classList.add("seller");
+            img.src = '';
+            sellerContainer.appendChild(img);
+            img.addEventListener("click",function(){
+                getSellerCategories(response.data[i].id)
+            },false)
+        }
+    });
+    
 }
 window.addEventListener("load",getSellers);
