c21ead428a215e22764642b6bbd6b0fad674df7e
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-24 14:58:28 +0300

    add sign in client

--- frontend-client/js/landingPageScript.js
+++ frontend-client/js/landingPageScript.js
@@ -1,7 +1,12 @@
+//Initialize APIs
+const loginAPI = "http://localhost/backend/signin.php";
+const button = document.getElementById("login_btn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const error = document.getElementById("erorr_message");
 const signinModal = document.getElementById("signin_modal");
 const signinButton = document.getElementById("signin_btn");
 const close = document.getElementById("close");
-
 
 signinButton.onclick = function() {
     signinModal.style.display = "block";
@@ -16,3 +21,30 @@ window.onclick = function(event) {
     signinModal.style.display = "none";
   }
 }
+
+const login = () => {
+    const data = new FormData();
+    data.append("email", email.value);
+    data.append("password", password.value);
+
+    axios.post(loginAPI, data)
+    .then(function (response) {
+    if(response.error != null ){
+        error.textContent = response.error;
+        return;
+    }
+
+    localStorage.setItem("userID", response.data.id)
+    localStorage.setItem("token", response.data.token)
+
+    if(response.data.user_type == "client"){
+        window.location.replace("landingPage.html");
+    }
+    else{
+        window.location.replace("/../frontend-seller/main_page_seller.html");
+    }
+ }
+);
+}
+  
+button.addEventListener("click", login);
