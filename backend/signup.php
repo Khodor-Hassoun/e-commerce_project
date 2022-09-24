c1e544fbba5141610b288797b4c0eab7d699b1d5
HawraaYounes <hawraayounes0@gmail.com>
2022-09-24 21:05:01 +0300

    Adding email validation to signup ap

--- backend/signup.php
+++ backend/signup.php
@@ -15,7 +15,8 @@ $lastName = $_POST['lastName'];
 $address = $_POST['address'];
 $phoneNumber = $_POST['phoneNumber'];
 $password =hash('sha256', $_POST["password"]) ;
-$userTypeSeller = 2;
+$userTypeID = 3;
+$isBanned=0;
 
 //Check if the inputs are correct
 if(
@@ -24,18 +25,40 @@ if(
     || !isset($firstName) || empty($firstName)
     || !isset($lastName) || empty($lastName)
     || !isset($password) || empty($password)
+    || !isset($address) || empty($address)
+
 ) {
     http_response_code(400);
     echo json_encode(['Error' =>"400", "Message" => "Incomplete data"]);
     return;
 }     
-
-//Hash password
-$password = hash("sha256", $password);
-
+ //check if email and password already exists in database
+ $query = $mysqli->prepare("SELECT * FROM users WHERE email= ?");
+ $query->bind_param("s", $email);
+ $query->execute();
+ $query->store_result();
+ $num_rows = $query->num_rows;
+     
+ if($num_rows != 0){
+     http_response_code(400);
+     echo json_encode(['status' => 400,'message' => 'User Already Exists']); 
+     return;
+ }
+ //check if username is taken
+ $query = $mysqli->prepare("SELECT * FROM users WHERE username=?");
+ $query->bind_param("s", $username);
+ $query->execute();
+ $query->store_result();
+ $num_rows = $query->num_rows;
+     
+ if($num_rows != 0){
+     http_response_code(400);
+     echo json_encode(['status' => 400,'message' => 'Username taken']); 
+     return;
+ }
 //Prepare and execute SQL query to add a new user
 $query = $mysqli->prepare("INSERT INTO `users` (`username`, `password`, `email`, `first_name`, `last_name`, `address`, `phone_number`, `is_banned`, `user_type_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"); 
-$query->bind_param("ssss", $username, $email, $password, $birthDate);
+$query->bind_param("sssssssii", $username,$password, $email,  $firstName,$lastName,$address,$phoneNumber,$isBanned,$userTypeID);
 $query->execute();
 
 $userID = mysqli_insert_id($mysqli);
