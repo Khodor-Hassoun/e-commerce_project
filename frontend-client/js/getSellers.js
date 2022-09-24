54f7b3eb8889e131e590bbfe5d6aad26bbbbf374
HawraaYounes <hawraayounes0@gmail.com>
2022-09-25 01:24:54 +0300

    Getting random products to client landing page

--- frontend-client/js/getSellers.js
+++ frontend-client/js/getSellers.js
@@ -1,7 +1,9 @@
 const sellerContainer=document.querySelector(".sellers");
 const getSellerAPI="http://localhost/backend/getSellers.php";
 const getSellerCategoriesAPI="http://localhost/backend/getCategories.php";
+const getRandomProductsAPI="http://localhost/backend/getRandProducts.php";
 const categoriesUl=document.querySelector(".categories_list");
+const productsDiv=document.querySelector(".products");
 const getSellers=()=>{
     axios.get(getSellerAPI)
     .then(response =>  {
@@ -53,4 +55,39 @@ const getSellerCategories=(id)=>{
     });
     
 }
-window.addEventListener("load",getSellers);
+
+const getRandomProducts=()=>{
+    axios.get(getRandomProductsAPI)
+    .then(response =>  {
+        //Show error
+        if (response.error != null) {
+            console.log("error")
+            return
+        }
+        //Loop over the response
+        for(let i = 0; i < response.data.length; i++){
+            const product=document.createElement("div");
+            product.classList.add("product");
+            productsDiv.appendChild(product);
+            const image=document.createElement("img");
+            image.classList.add("product_image");
+            image.src = response.data[i].thumbnail;
+            product.appendChild(image);
+            const productDesc=document.createElement("div");
+            productDesc.classList.add("product_desc");
+            product.appendChild(productDesc);
+            const h4=document.createElement("h4");
+            h4.innerText=response.data[i].name;
+            const span=document.createElement("span");
+            span.innerText=response.data[i].price;
+            productDesc.appendChild(h4);
+            productDesc.appendChild(span);
+
+        }
+    });
+}
+const loadPage=()=>{
+    getSellers();
+    getRandomProducts();
+}
+window.addEventListener("load",loadPage);
