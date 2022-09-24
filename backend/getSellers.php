c5a06637dbd6e5ec2edc4eceb001ac27a20bf9fb
HawraaYounes <hawraayounes0@gmail.com>
2022-09-24 23:35:04 +0300

    Editing getSellers api

--- backend/getSellers.php
+++ backend/getSellers.php
@@ -1,15 +1,18 @@
 <?php
     require_once("headers.php");
     include("connection.php");
+    // require_once("jwtFunc.php");
+    // //Check JWT token
+    // if(!jwtAuth()){
+    //     return;
+    // }
 
-    //Execute a query to get all sellers
-    $query = $mysqli->prepare("SELECT * FROM users WHERE user_type_id =2");
+    $query = $mysqli->prepare("SELECT * FROM users WHERE user_type =2");
     $query->execute();
     $array = $query->get_result();
 
     $response = [];
 
-    //Save data in an array
     while($a = $array->fetch_assoc()){
         $response[] = $a;
     }
