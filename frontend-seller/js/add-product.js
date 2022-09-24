f7512922535caeeeed539873d96afd3ef6632823
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-24 18:17:59 +0300

    Insert categories automatically

--- frontend-seller/js/add-product.js
+++ frontend-seller/js/add-product.js
@@ -3,7 +3,8 @@ const backBtn = document.querySelector('.close-btn-pop');
 const popupContainer = document.querySelector('.popup-container');
 const thumbDiv = document.querySelector('.thumbnail-div')
 
-const addpProductApi = 'http://localhost/e-commerce_project/backend/addProductByCategory.php.php'
+const getCategory = "http://localhost/e-commerce_project/backend/getCategories.php"
+const addpProductApi = 'http://localhost/e-commerce_project/backend/addProductByCategory.php'
 const thumbnail = document.getElementById('thumbnail');
 const prodName = document.querySelector('#name');
 const description = document.querySelector('#desc')
@@ -17,6 +18,27 @@ const config={
         Authorization: localStorage.getItem("token")
     }
 }
+
+// Get the categories from getCategory and place them in category select form
+axios.get(getCategory,{
+    params: {
+		id: 9
+	}
+})
+.then(res=>{
+    // res.data returns an array of objects
+    console.log(res.data)
+    for(let object of res.data){
+        const catOption = document.createElement('option')
+        // get category id
+        catOption.value = object.id
+
+        // get innerText
+        catOption.textContent = `${object.name}`
+        catName.append(catOption)
+    }
+})
+
 
 thumbnail.addEventListener('change',()=>{
     const file = thumbnail.files[0]
