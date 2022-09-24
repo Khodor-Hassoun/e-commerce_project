a32227fec20c62e7a7859ab577258c0db9e23714
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-24 15:54:20 +0300

    finish login client

--- frontend-client/js/landingPageScript.js
+++ frontend-client/js/landingPageScript.js
@@ -3,10 +3,12 @@ const loginAPI = "http://localhost/backend/signin.php";
 const button = document.getElementById("login_btn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const error = document.getElementById("erorr_message");
+const error = document.getElementById("error");
 const signinModal = document.getElementById("signin_modal");
 const signinButton = document.getElementById("signin_btn");
 const close = document.getElementById("close");
+
+if(localStorage.getItem("userID"))
 
 signinButton.onclick = function() {
     signinModal.style.display = "block";
@@ -29,19 +31,29 @@ const login = () => {
 
     axios.post(loginAPI, data)
     .then(function (response) {
-    if(response.error != null ){
-        error.textContent = response.error;
+    if(response.message != null ){
+        error.textContent = "Invalid input";
         return;
     }
 
     localStorage.setItem("userID", response.data.id)
     localStorage.setItem("token", response.data.token)
 
-    if(response.data.user_type == "client"){
+    console.log(response.data);
+
+    if(response.data.is_banned == 1){
+        error.textContent = "Banned account";
+        return;
+    }
+
+    if(response.data.type == "client"){
         window.location.replace("landingPage.html");
     }
-    else{
+    else if (response.data.type == "seller"){
         window.location.replace("/../frontend-seller/main_page_seller.html");
+    }
+    else{
+        error.textContent = "Invalid input";
     }
  }
 );
