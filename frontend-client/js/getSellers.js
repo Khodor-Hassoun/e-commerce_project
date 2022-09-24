fe8e1e4c87995e40baa3938c3eed343f076ef5b1
HawraaYounes <hawraayounes0@gmail.com>
2022-09-24 23:25:31 +0300

    Getting sellers from getSellers api to client landing page

---
+++ frontend-client/js/getSellers.js
@@ -0,0 +1,22 @@
+const sellerContainer=document.querySelector(".sellers");
+const getSellerAPI="http://localhost/backend/getSellers.php";
+const getSellers=()=>{
+    axios.get(getSellerAPI)
+    .then(response =>  {
+        console.log(response.data)
+        //Show error
+        if (response.error != null) {
+            console.log("error")
+            return
+        }
+        //Loop over the response
+        for(let i = 0; i < response.data.length; i++){
+            const img=document.createElement("img");
+            img.classList.add("seller");
+            img.src = '';
+            sellerContainer.appendChild(img);
+
+        }
+    });
+}
+window.addEventListener("load",getSellers);
