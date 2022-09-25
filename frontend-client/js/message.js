83c8a9c6cc8d936d008318e10eb6580b1e2dafc5
HawraaYounes <hawraayounes0@gmail.com>
2022-09-25 15:18:52 +0300

    Commenting the code

--- frontend-client/js/message.js
+++ frontend-client/js/message.js
@@ -4,7 +4,7 @@ const getSellerAPI="http://localhost/backend/getSellers.php";
 const getMessagesAPI="http://localhost/backend/getMessages.php";
 const addMessageAPI="http://localhost/backend/addMessage.php";
 
-const getSellers=()=>{
+const getSellers=()=>{//get sellers and show them in sellers div
     axios.get(getSellerAPI)
     .then(response =>  {
         //Show error
@@ -15,14 +15,13 @@ const getSellers=()=>{
         console.log(response.data)
         //Loop over the response
         response.data.forEach(element => {
-            console.log(element)
             const seller=document.createElement("div");
             sellersDiv.appendChild(seller);
             seller.innerText=element.username;
             seller.style.backgroundColor="#1F7A8C";
             seller.style.margin="1px";
             seller.classList.add("seller-row");
-            seller.addEventListener("click",function(){
+            seller.addEventListener("click",function(){//on clicking on seller row, client is able to start chat
                 startChat(element.id);
 
             });
@@ -32,21 +31,23 @@ const getSellers=()=>{
     }      
 
 const startChat=(sellerid)=>{
-    alert(sellerid)
     
     const btmDiv=document.createElement("div");
     btmDiv.classList.add("input-msg")
-    
-    const inputMessage=document.createElement("input");
+    const inputMessage=document.createElement("INPUT");
+    inputMessage.setAttribute("type", "text");
     inputMessage.classList.add("message-input");
     inputMessage.placeholder="Type Your Message Here..";
+    const inputValue=inputMessage.value;
+    console.log(inputValue)
     btmDiv.appendChild(inputMessage);
+    
     const sendBtn=document.createElement("button");
     sendBtn.classList.add("send-btn");
     sendBtn.innerText="Send";
     sendBtn.id=sellerid;
-    sendBtn.addEventListener("click",function(){
-        sendMessage(sellerid)
+    sendBtn.addEventListener("click",function(){//on clicking on send button, sendMessage function will be executed
+        sendMessage(sellerid,inputValue)
        })
     btmDiv.appendChild(sendBtn);
     const data = new FormData();
@@ -61,13 +62,13 @@ const startChat=(sellerid)=>{
             return
         }
         
-        if(response.data.length==0){
+        if(response.data.length==0){//if no old messages found, display No Messages
             chatDiv.innerText=" "
             chatDiv.innerText="No messages"
             chatDiv.style.color="white"
             chatDiv.appendChild(btmDiv)
     }
-        //Loop over the response
+        //if there are old messages,display them
         else{
             chatDiv.innerText=" ";
             for(let i = 0; i < response.data.length; i++){
@@ -85,11 +86,11 @@ const startChat=(sellerid)=>{
 
 }
 
-const sendMessage=(id)=>{
+const sendMessage=(id,value)=>{//post input data and user ids to database
     const data = new FormData();
    data.append("sender_id",1)
     data.append("reciever_id", id);
-    data.append("message", inputMessage.value);
+    data.append("message", value);
     axios.post(addMessageAPI,data)
     .then(response =>  {
         //Show error
@@ -97,7 +98,9 @@ const sendMessage=(id)=>{
             console.log("error")
             return
         }
-        //Loop over the response
+        else{
+            alert("sent")
+        }
         
     });
 }
