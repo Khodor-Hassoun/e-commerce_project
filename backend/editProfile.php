669d9d7e37127051b4b08d85964d290555d77d9e
Zeina Olabi <52928499+zeinaolabi@users.noreply.github.com>
2022-09-25 12:02:57 +0300

    remove password from edit profile

--- backend/editProfile.php
+++ backend/editProfile.php
@@ -10,14 +10,13 @@
 
     $id = $_POST["id"];
     $username = $_POST["username"];
-    $password= hash('sha256', $_POST["password"]) ;
     $email = $_POST["email"];
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
 
     //Check if ID's are not empty
     if(!isset($id) || empty($id) || !isset($username) || empty($username)
-        || !isset($password) || empty($password) || !isset($email) || empty($email)
+        || !isset($email) || empty($email)
         || !isset($first_name) || empty($first_name) || !isset($last_name) || empty($last_name)){ 
         http_response_code(400);
         echo json_encode([
@@ -54,8 +53,8 @@
     }
 
     // Update User
-    $query = $mysqli->prepare("UPDATE users SET first_name= ?, last_name= ?,username= ?,email= ?,password= ? WHERE id= ?");
-    $query->bind_param('sssssi', $first_name, $last_name, $username, $email, $password, $id);
+    $query = $mysqli->prepare("UPDATE users SET first_name= ?, last_name= ?,username= ?,email= ? WHERE id= ?");
+    $query->bind_param('ssssi', $first_name, $last_name, $username, $email, $id);
     $query->execute();
     echo json_encode(["message" => "success"]);
 
