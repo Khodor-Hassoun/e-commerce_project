f410f6f27e5acc9080fb0d88de46b8b15dccb993
HawraaYounes <hawraayounes0@gmail.com>
2022-09-25 13:25:59 +0300

    Fixing error in sendMessage function

--- frontend-client/js/message.js
+++ frontend-client/js/message.js
@@ -3,6 +3,7 @@ const chatDiv=document.querySelector("#chat");
 const getSellerAPI="http://localhost/backend/getSellers.php";
 const getMessagesAPI="http://localhost/backend/getMessages.php";
 
+
 const getSellers=()=>{
     axios.get(getSellerAPI)
     .then(response =>  {
@@ -11,27 +12,47 @@ const getSellers=()=>{
             console.log("error")
             return
         }
+        console.log(response.data)
         //Loop over the response
-        for(let i = 0; i < response.data.length; i++){
-           const seller=document.createElement("div");
-           sellersDiv.appendChild(seller);
-           seller.innerText=response.data[i].username;
-           seller.style.backgroundColor="#1F7A8C";
-           seller.style.margin="1px";
-           seller.classList.add("seller-row");
-           seller.addEventListener("click",function(){
-            startChat(response.data[i].id);
-           });
-           sendBtn.addEventListener("click",function(){
-            sendMessage(response.data[i].id)
-           })
-        }
-    });
-}
-const startChat=(id)=>{
+        response.data.forEach(element => {
+            console.log(element)
+            const seller=document.createElement("div");
+            sellersDiv.appendChild(seller);
+            seller.innerText=element.username;
+            seller.style.backgroundColor="#1F7A8C";
+            seller.style.margin="1px";
+            seller.classList.add("seller-row");
+            seller.addEventListener("click",function(){
+                startChat(element.id);
+
+            });
+        });
+        });
+           
+    }      
+
+const startChat=(sellerid)=>{
+    alert(sellerid)
+    
+    const btmDiv=document.createElement("div");
+    btmDiv.classList.add("input-msg")
+    
+    const inputMessage=document.createElement("input");
+    inputMessage.classList.add("message-input");
+    inputMessage.placeholder="Type Your Message Here..";
+    btmDiv.appendChild(inputMessage);
+    const sendBtn=document.createElement("button");
+    sendBtn.classList.add("send-btn");
+    sendBtn.innerText="Send";
+    sendBtn.id=sellerid;
+    sendBtn.addEventListener("click",function(){
+        sendMessage(sellerid)
+       })
+    btmDiv.appendChild(sendBtn);
     const data = new FormData();
-    data.append("seller_id", id);
-    data.append("user_id", 41);
+    data.append("seller_id", sellerid);
+    data.append("user_id", 41)
+    
     axios.post(getMessagesAPI,data)
     .then(response =>  {
         //Show error
@@ -39,17 +60,6 @@ const startChat=(id)=>{
             console.log("error")
             return
         }
-        const btmDiv=document.createElement("div");
-        btmDiv.classList.add("input-msg")
-        
-        const inputMessage=document.createElement("input");
-        inputMessage.classList.add("message-input");
-        inputMessage.placeholder="Type Your Message Here..";
-        btmDiv.appendChild(inputMessage);
-        const sendBtn=document.createElement("button");
-        sendBtn.classList.add("send-btn");
-        sendBtn.innerText="Send";
-        btmDiv.appendChild(sendBtn);
         
         if(response.data.length==0){
             chatDiv.innerText=" "
@@ -59,15 +69,11 @@ const startChat=(id)=>{
     }
         //Loop over the response
         else{
-            
             chatDiv.innerText=" ";
             for(let i = 0; i < response.data.length; i++){
-                sendBtn.addEventListener("click",function(){
-                    sendMessage(response.data[i].id)
-                });
                 const message=document.createElement("div");
                 message.classList.add("message");
-                if((response.data[i].sender_id==id && response.data[i].reciever_id==41) || (response.data[i].sender_id==41 && response.data[i].reciever_id==id)){
+                if((response.data[i].sender_id==sellerid && response.data[i].reciever_id==41) || (response.data[i].sender_id==41 && response.data[i].reciever_id==sellerid)){
                      message.innerText=response.data[i].message;
                     chatDiv.appendChild(message)
                 }
