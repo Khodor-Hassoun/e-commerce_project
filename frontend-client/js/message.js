84466f05f1befaa18cb2aeea3f5f7e206e720739
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-25 21:45:26 +0300

    clean code

--- frontend-client/js/message.js
+++ frontend-client/js/message.js
@@ -2,13 +2,21 @@
 const getSellerAPI="http://localhost/backend/getSellers.php";
 const getMessagesAPI="http://localhost/backend/getMessages.php";
 const addMessageAPI="http://localhost/backend/addMessage.php";
-//initializing variables
-const sellersDiv=document.querySelector("#getSellers");
-const chatDiv=document.querySelector("#chat");
-let inputValue;
-const userID=localStorage.getItem("userID");
 
-const getSellers=()=>{//get sellers and show them in sellers div
+//initializing variables
+const sellersDiv = document.querySelector("#getSellers");
+const chatDiv = document.querySelector("#chat");
+const userID = localStorage.getItem("userID");
+let inputValue;
+const config = {
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+}
+
+const getSellers=()=>{
+
+    //get sellers and show them in sellers div
     axios.get(getSellerAPI)
     .then(response =>  {
         //Show error
@@ -16,44 +24,52 @@ const getSellers=()=>{//get sellers and show them in sellers div
             console.log("error")
             return
         }
-        console.log(response.data)
+
         //Loop over the response
         response.data.forEach(element => {
             const seller=document.createElement("div");
             sellersDiv.appendChild(seller);
             seller.innerText=element.username;
             seller.classList.add("slr");
-            seller.addEventListener("click",function(){//on clicking on seller row, client is able to start chat
+
+            //on clicking on seller row, client is able to start chat
+            seller.addEventListener("click",function(){
                 startChat(element.id);
 
             });
         });
-        });
-           
-    }      
+    });     
+}      
 
 const startChat=(sellerid)=>{
     const btmDiv=document.createElement("div");
     btmDiv.classList.add("input-msg")
     const inputMessage=document.createElement("INPUT");
+
     inputMessage.setAttribute("type", "text");
     inputMessage.classList.add("message-input");
     inputMessage.placeholder="Type Your Message Here..";
+
     btmDiv.appendChild(inputMessage);
     const sendBtn=document.createElement("button");
+
     sendBtn.classList.add("send-btn");
     sendBtn.innerText="Send";
     sendBtn.id=sellerid;
-    sendBtn.addEventListener("click",function(){//on clicking on send button, sendMessage function will be executed
+
+    //on clicking on send button, sendMessage function will be executed
+    sendBtn.addEventListener("click",function(){
         inputValue=inputMessage.value;
         sendMessage(sellerid,inputValue)
        })
+
     btmDiv.appendChild(sendBtn);
     const data = new FormData();
+
     data.append("seller_id", sellerid);
     data.append("user_id", userID)
     
-    axios.post(getMessagesAPI,data)
+    axios.post(getMessagesAPI,data, config)
     .then(response =>  {
         //Show error
         if (response.error != null) {
@@ -61,12 +77,13 @@ const startChat=(sellerid)=>{
             return
         }
         
-        if(response.data.length==0){//if no old messages found, display No Messages
+        if(response.data.length==0){
+            //if no old messages found, display No Messages
             chatDiv.innerText=" "
             chatDiv.innerText="No messages";
             chatDiv.classList.add("no-msgs");
             chatDiv.appendChild(btmDiv)
-    }
+        }
         //if there are old messages,display them
         else{
             chatDiv.innerText=" ";
@@ -74,41 +91,44 @@ const startChat=(sellerid)=>{
             for(let i = 0; i < response.data.length; i++){
                 const message=document.createElement("div");
                 message.classList.add("message");
+                
                 //select from messages that are between loggedin user and clicked seller
                 if((response.data[i].sender_id==sellerid && response.data[i].reciever_id==userID) || (response.data[i].sender_id==userID && response.data[i].reciever_id==sellerid)){
-                     if(response.data[i].sender_id==userID){//if message is for loggedin user
-                            message.innerText=response.data[i].message;//display it and give it custom class
+                    //if message is for loggedin user
+                    if(response.data[i].sender_id==userID){
+                        //display it and give it custom class
+                            message.innerText=response.data[i].message;
                             chatDiv.appendChild(message)
                         
                     }
-                    else{//if message is for clicked seller
-                        message.innerText=response.data[i].message;//display it and give it different class
+                    else{
+                        //if message is for clicked seller
+                        //display it and give it different class
+                        message.innerText=response.data[i].message;
                         message.classList.add("light-blue-msg");
                         chatDiv.appendChild(message)
                     }
                 }
-                
             } 
-            chatDiv.appendChild(btmDiv)//add input and button Div at the bottom of the page
+            //add input and button Div at the bottom of the page
+            chatDiv.appendChild(btmDiv)
       }
     });
 
 }
 
-const sendMessage=(id,value)=>{//post input data and user ids to database
+const sendMessage=(id,value)=>{
+    //post input data and user ids to database
     const data = new FormData();
-   data.append("sender_id",userID)
+    data.append("sender_id",userID)
     data.append("reciever_id", id);
     data.append("message", value);
-    axios.post(addMessageAPI,data)
-    .then(response =>  {
-        //Show error
-        if (response.error != null) {
-            console.log("error")
-            return
-        }
-       
-        
+
+    axios.post(addMessageAPI,data, config)
+    .catch((e)=>{
+        console.log(e)
     });
 }
-window.addEventListener("load",getSellers)//when page load,execute get sellers function
+
+//when page load,execute get sellers function
+window.addEventListener("load",getSellers)
