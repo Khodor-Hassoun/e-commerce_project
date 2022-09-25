c96c91d5ae0164b31646382240a5f40fe0e75ca0
HawraaYounes <hawraayounes0@gmail.com>
2022-09-25 20:52:17 +0300

    Updating getWishlistItems api

--- backend/getWishlistItems.php
+++ backend/getWishlistItems.php
@@ -2,16 +2,31 @@
     require_once("headers.php");
     include("connection.php");
 
-    $user_id= $_GET["user_id"];
-    $product_id = $_GET["product_id"];
-    if(!isset($user_id) || empty($user_id) || !isset($product_id) || empty($product_id)){ 
-        echo json_encode(['status' => 400,'message' => 'IDs cannot be empty']); 
-        return;
+        $user_id= $_GET["user_id"];
+       
+        //check if ID's empty
+        if(!isset($user_id) || empty($user_id)){ 
+            echo json_encode(['status' => 400,'message' => 'IDs cannot be empty']); 
+            return;
         } 
+        //check if there's wishlist items
+        $query = $mysqli->prepare("SELECT * FROM wishlists WHERE user_id = ?");
+        $query->bind_param("i", $user_id);
+        $query->execute();
+        $res=$query->store_result();
+        $num_rows = $query->num_rows;
+    
+        //if no,send a message that their is no wishlist items
+        if ($num_rows==0) {
+            http_response_code(400);
+            echo json_encode(['status' => 400,'message' => 'No Wishlist items found']);
+            return;
+        }
         
 
-    $query = $mysqli->prepare("SELECT * FROM wishlists  WHERE user_id=? and product_id=?");
-    $query->bind_param("ii", $user_id,$product_id);
+    $query = $mysqli->prepare("Select * FROM wishlists, products
+    WHERE wishlists.product_id = products.id and wishlists.user_id = ?");
+    $query->bind_param("i", $user_id);
     $query->execute();
     $array = $query->get_result();
 
