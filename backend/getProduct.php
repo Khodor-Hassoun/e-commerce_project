6796ab1dae0c181f3b6f81981d63b1e86aaa99f3
Zeina Olabi <52928499+zeinaolabi@users.noreply.github.com>
2022-09-25 17:28:37 +0300

    fix get product

--- backend/getProduct.php
+++ backend/getProduct.php
@@ -3,14 +3,8 @@
     // DataBase connection
     include("connection.php");
     require_once("headers.php");
-    require_once("jwtFunc.php");
 
-    //Check JWT token
-    if(!jwtAuth()){
-        return;
-    }
-
-    $product_id = $_POST["product_id"];
+    $product_id = $_GET["product_id"];
 
     //Validate product id
     if(!isset($product_id) || empty($product_id)){
@@ -25,23 +19,13 @@
     //Prepare and execute SQL query to retrieve product's record
     $query = $mysqli->prepare(
         "SELECT * FROM products P
-        WHERE P.ID = (?)");
+        WHERE P.id = (?)");
 
     $query->bind_param("i", $product_id);
     $query->execute();
+    
 
     $response = $query->get_result()->fetch_assoc();
-
-    //If response is empty, send back an error message
-    if (empty($response)) {
-        http_response_code(400);
-        echo json_encode([
-            'error' => 400,
-            'message' => 'unable to retrieve products'
-        ]);
-
-        return;
-    }
 
     echo json_encode($response);
 
