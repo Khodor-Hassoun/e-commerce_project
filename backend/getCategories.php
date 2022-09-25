10c7a4469099b49d398356c15ede041609693010
Khodor Hassoun <111639413+Khodor-Hassoun@users.noreply.github.com>
2022-09-25 17:41:46 +0300

    Add JWT

--- backend/getCategories.php
+++ backend/getCategories.php
@@ -2,6 +2,12 @@
     // DataBase connection
     include("connection.php");
     require_once("headers.php");
+    require_once("jwtFunc.php");
+
+    //Check JWT token
+    if(!jwtAuth()){
+        return;
+    }
 
     $seller_id = $_GET['id'];
 
