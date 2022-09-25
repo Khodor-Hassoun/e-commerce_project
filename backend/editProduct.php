0a5c796d59f32a6c1e0aac1027d2ff929b5b6f45
Khodor Hassoun <111639413+Khodor-Hassoun@users.noreply.github.com>
2022-09-25 17:39:39 +0300

    Correct JWT

--- backend/editProduct.php
+++ backend/editProduct.php
@@ -1,12 +1,12 @@
 <?php
     include("headers.php");
     include("connection.php");
-    // require_once("jwtFunc.php");
+    require_once("jwtFunc.php");
 
     //Check JWT token
-    // if(!jwtAuth()){
-    //     return;
-    // }
+    if(!jwtAuth()){
+        return;
+    }
 
     $id = $_POST["id"];
     $thumbnail = $_POST['thumbnail'];
